c08e54058ae65c617cbc50755b19e01d87fcdcec
VladYem <volodymyem@gmail.com>
2020-02-16 05:33:30 +0100

    JS fixes due to responsive design

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let percentNumber = document.querySelector('#percent-number');
 let actualValue = document.querySelector('#actual-value').innerHTML;
 let neededValue = document.querySelector('#needed-value').innerHTML;
 let progressbar = document.querySelector('#progressbar-percent');
+let progressbarMobile = document.querySelector('#progressbar-mobile');
+let percentNumberMobile = document.querySelector('#percent-number-mobile');
+
 actualValue = actualValue.replace(/\s/g, '');
 neededValue = neededValue.replace(/\s/g, '');
 let numberActual = parseInt(actualValue);
@@ -16,5 +19,7 @@ function percentDone(needed, actual) {
 percent = percentDone(numberActual, numberNeeded);
 
 progressbar.style.width = percent + '%';
-
 percentNumber.innerHTML = Math.round(percent) + '%';
+
+progressbarMobile.style.width = percent + '%';
+percentNumberMobile.innerHTML = Math.round(percent) + '%';
